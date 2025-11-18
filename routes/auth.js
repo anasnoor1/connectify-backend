ce8fc3b63e903178ae09a750b9b7e36284a4a43f
naeem123r <naeemilyas2140@gmail.com>
2025-11-18 23:52:02 +0500

    aa

--- routes/auth.js
+++ routes/auth.js
@@ -12,5 +12,6 @@ router.post('/password/forgot', authController.passwordForgot);
 router.post('/password/verify-reset', authController.verifyResetOtp);
 router.put('/password/update', authController.updatePassword);
 router.get("/counts", authController.getCounts);
+router.post('/check-email', authController.checkEmail);
 
 module.exports = router;
